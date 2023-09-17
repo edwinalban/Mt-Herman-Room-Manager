1240941aef59f1beca88168e69374d4736141e3e
Yachtzee <edwinalban@gmail.com>
2023-09-17 09:17:16 -0700

    updated seeds/index.js to include lakesideRoomsSeeds

--- seeds/index.js
+++ seeds/index.js
@@ -1,4 +1,6 @@
 const lakesideSeedRooms = require('./lakesideRoomsSeeds');
 const seedEmployees = require('./employeeSeeds');
 
-module.exports = {lakesideSeedRooms, seedEmployees};
+const seedRooms = lakesideSeedRooms;
+
+module.exports = {seedRooms, seedEmployees};
