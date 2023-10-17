91b702e3224619817d3cde98e350012d61c3aafc
Yachtzee <edwinalban@gmail.com>
2023-10-16 23:39:33 -0700

    removed cruft

--- routes/api/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const employeeRoutes = require('./employeeRoutes');
-const roomRoutes = require('./roomRoutes');
-
-router.use('/employees', employeeRoutes);
-router.use('/rooms', roomRoutes);
-
-module.exports = router;
