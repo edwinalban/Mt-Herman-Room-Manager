1a02510154a0a6f36862f69a2327d0c35a69a06c
Yachtzee <edwinalban@gmail.com>
2024-04-03 00:35:05 -0700

    updated connection.js with development db info

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://edwinalban:0GfVT9WGKwVtnAzn@cluster0.938sxem.mongodb.net/roomManagerDB?retryWrites=true&w=majority');
+mongoose.connect('mongodb://127.0.0.1:27017/roomManagerDB');
 
 module.exports = mongoose.connection;
